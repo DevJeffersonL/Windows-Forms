182f816a18bf6ef5f8a16c05c036e7328abdd83c
DevJefferson <88711378+DevJeffersonL@users.noreply.github.com>
2022-04-13 21:33:45 +0100

    1

---
+++ Sorting Algorithm/Linear Search/code_main/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Dim myArray(10)
+    Dim random As New Random()
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Clear()
+        For i As Integer = 0 To myArray.Length - 1
+            myArray(i) = random.Next(0, 10)
+            TextBox1.AppendText(Str(myArray(i)))
+        Next
+        Array.Sort(myArray)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox2.Clear()
+        For j As Integer = 0 To myArray.Length - 1
+            TextBox2.AppendText(Str(myArray(j)))
+        Next
+    End Sub
+End Class
